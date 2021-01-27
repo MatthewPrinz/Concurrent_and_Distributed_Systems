f725dcebdbe538e12e63f61cd489704cea7b02fd
Matthew Prinz <mattprinz@gmail.com>
2021-01-27 13:49:15 -0600

    binary search method added

--- hw1-template/PMerge.java
+++ hw1-template/PMerge.java
@@ -9,4 +9,23 @@ public class PMerge{
   public static void parallelMerge(int[] A, int[] B, int[]C, int numThreads){
 
   }
+
+  public static int binarySearch(int[] A, int T)
+  {
+    int L = 0;
+    int R = A.length - 1;
+    while (L < R)
+    {
+      int m = (L+R)/2;
+      if (A[m] < T)
+        L = m+1;
+      else if (A[m] > T)
+        R = m+1;
+      else
+        return m;
+    }
+    return 0;
+  }
+
+
 }
