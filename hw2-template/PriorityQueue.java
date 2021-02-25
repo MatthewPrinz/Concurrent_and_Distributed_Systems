3a2309c368b170b7c0029d4ea29ecfd4b1749f74
Matthew Prinz <mattprinz@gmail.com>
2021-02-25 17:42:16 -0600

    checked order and some randomness, seems good

--- hw2-template/PriorityQueue.java
+++ hw2-template/PriorityQueue.java
@@ -1,114 +1,139 @@
 // EID 1
 // EID 2
 
+import java.util.HashSet;
+import java.util.Set;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.ReentrantLock;
 
 public class PriorityQueue {
-	final static int MIN_PRIORITY = 0;
-	final static int MAX_PRIORITY = 9;
+    final static int MIN_PRIORITY = 0;
+    final static int MAX_PRIORITY = 9;
 
-	// Underlying Linked List for Priority Queue
-	Node llHead;
-	AtomicInteger currSize = new AtomicInteger(0);
-	int maxSize;
+    // Underlying Linked List for Priority Queue
+    Node head;
+    AtomicInteger currSize = new AtomicInteger(0);
+    int maxSize;
 
-	// Locks and conditions for concurrency
-	ReentrantLock capacityLock = new ReentrantLock();
-	Condition notEmpty = capacityLock.newCondition();
-	Condition notFull = capacityLock.newCondition();
+    // Locks and conditions for concurrency
+    ReentrantLock capacityLock = new ReentrantLock();
+    final Condition empty = capacityLock.newCondition();
+    final Condition full = capacityLock.newCondition();
+    Set<String> names = new HashSet<>();
 
+    // implement fine-grained synchronization with lock on each node
+    class Node {
+        String name;
+        int priority;
+        Node next;
+        ReentrantLock rel = new ReentrantLock();
 
-	// implement fine-grained synchronization with lock on each node
-	class Node {
-		String name;
-		int priority;
-		Node next;
-		ReentrantLock rel = new ReentrantLock();
+        Node(String name, int priority, Node next) {
+            this.name = name;
+            this.priority = priority;
+            this.next = next;
+        }
+    }
 
-		Node (String name, int priority, Node next) {
-			this.name = name;
-			this.priority = priority;
-			this.next = next;
-		}
-	}
-
-	public PriorityQueue(int maxSize) {
+    public PriorityQueue(int maxSize) {
         // Creates a Priority queue with maximum allowed size as capacity
-		// Initialize dummy variables at both start and end of Linked List
-		this.llHead = new Node("", MAX_PRIORITY+1, null);
-		this.llHead.next = new Node("", MIN_PRIORITY+1, null);
-		this.maxSize = maxSize;
-	}
+        // Initialize dummy variables at both start and end of Linked List
+        this.head = new Node("", MAX_PRIORITY + 1, null);
+        this.maxSize = maxSize;
+    }
 
-	public int add(String name, int priority) {
+    public int add(String name, int priority) {
         // Adds the name with its priority to this queue.
         // Returns the current position in the list where the name was inserted;
         // otherwise, returns -1 if the name is already present in the list.
 
-		// check for non-full Linked List
-		capacityLock.lock();
-		while (currSize.get() >= maxSize) {
-			try {
-				notFull.await();
-			} catch (InterruptedException e) {
-				System.out.println("Caught interrupted exception");
-			}
-		}
-		currSize.incrementAndGet();
-		notEmpty.signal();
-		capacityLock.unlock();
+        // check for non-full Linked List
+        capacityLock.lock();
+        while (currSize.get() == maxSize) {
+            try {
+                full.await();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        empty.signalAll();
+        if (names.contains(name))
+            return -1;
+        capacityLock.unlock();
+        int pos = -1; // -1 because of our head
+        Node temp = head;
 
-		// This method blocks when the list is full.
-		Node prev = llHead;
-		prev.rel.lock();
-		Node curr = llHead.next;
-		curr.rel.lock();
-
-		// traverse to correct node (locking pairs along the way)
-		int pos = 0;
-		while (curr.priority <= priority) {
-			if (name.equals(curr.name)) {
-				prev.rel.unlock();
-				curr.rel.unlock();
-
-				// decrement size bc add unsuccessful
-				capacityLock.lock();
-				currSize.decrementAndGet();
-				notFull.notify();
-				capacityLock.unlock();
-				return -1;
-			}
-
-			pos++;
-			prev.rel.unlock();
-			prev = curr;
-			curr = curr.next;
-			curr.rel.lock();
-		}
+        while (temp.priority > priority) {
+            pos++;
+            if (temp.next != null && temp.next.priority >= priority) {
+                temp = temp.next;
+            } else
+                break;
+        }
+        try {
+            temp.rel.lock();
+            Node newNode;
+            if (pos == currSize.get()) {
+                newNode = new Node(name, priority, null);
+            } else {
+                newNode = new Node(name, priority, temp.next);
+            }
+            temp.next = newNode;
+            this.names.add(name);
+            currSize.incrementAndGet();
+        } finally {
+            temp.rel.unlock();
+        }
+        return pos;
+    }
 
 
-		// create and add node
-		prev.next = new Node(name, priority, curr);
-		prev.rel.unlock();
-		curr.rel.unlock();
-
-		return pos;
-	}
-
-	public int search(String name) {
+    public int search(String name) {
         // Returns the position of the name in the list;
         // otherwise, returns -1 if the name is not found.
-		Node temp = this.llHead;
+        Node temp = this.head;
+        int i = -1; // -1 because we have a MaxPrio + 1 head
+        while (temp != null) {
+            if (name.equals(temp.name) && i != -1) {
+                return i;
+            }
+            i++;
+            temp = temp.next;
+        }
+        return -1;
+    }
 
-		return 1;
-	}
-
-	public String getFirst() {
+    public String getFirst() {
         // Retrieves and removes the name with the highest priority in the list,
         // or blocks the thread if the list is empty.
-		return "";
-	}
+        capacityLock.lock();
+        try {
+            while (currSize.get() == 0) {
+                try {
+                    empty.await();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            head.rel.lock();
+            String removed;
+            try {
+                removed = head.next.name;
+                names.remove(head.next.name);
+                if (currSize.get() >= 2)
+                    head.next = this.head.next.next;
+                else
+                    head.next = null;
+                currSize.decrementAndGet();
+            } finally {
+                head.rel.unlock();
+            }
+            full.signalAll();
+            return removed;
+        } finally {
+            capacityLock.unlock();
+        }
+    }
 }
 
--- hw2-template/testPriorityQueue.java
+++ hw2-template/testPriorityQueue.java
@@ -1,8 +1,8 @@
+import java.util.Random;
 
 public class testPriorityQueue implements Runnable {
-    final static int SIZE = 30;
-    final static int ROUND = 5;
-
+    final static int NUMTHREADS = 4;
+    final static int PQUEUESIZE = 2;
     // final CyclicBarrier gate;
     final PriorityQueue pQueue;
 
@@ -11,23 +11,57 @@ public class testPriorityQueue implements Runnable {
     }
 
     public void run() {
-        for (int i = 0; i < ROUND; ++i) {
-
-        }
+        // bound is exclusive
+        int priority = Integer.parseInt(Thread.currentThread().getName()) % 9;
+        System.out.println("Thread " + Thread.currentThread().getName() + " is adding Node(" + Thread.currentThread().getName() + ", " + priority + ")");
+        pQueue.add(Thread.currentThread().getName(), priority);
     }
 
     public static void main(String[] args) {
-        PriorityQueue pQueue = new PriorityQueue(SIZE);
-		MonitorCyclicBarrier gate = new MonitorCyclicBarrier(SIZE);
-
-        Thread[] t = new Thread[SIZE];
-
-        for (int i = 0; i < SIZE; ++i) {
-            t[i] = new Thread(new testCyclicBarrier(gate));
+        PriorityQueue pQueue = new PriorityQueue(PQUEUESIZE);
+        testReal(pQueue);
+    }
+    public static void testAdds(PriorityQueue pQueue)
+    {
+        pQueue.add("C", 7);
+        pQueue.add("B", 8);
+        pQueue.add("D", 5);
+        pQueue.add("A", 9);
+        for (int i = 0 ; i < 4; i++)
+        {
+            System.out.println(pQueue.getFirst());
         }
-
-        for (int i = 0; i < SIZE; ++i) {
+    }
+    // make it more realistic
+    public static void testReal(PriorityQueue pQueue)
+    {
+        final Random random = new Random(5);
+        Thread[] t = new Thread[NUMTHREADS];
+        for (int i = 0; i < NUMTHREADS; ++i) {
+            t[i] = new Thread(new testPriorityQueue(pQueue));
+            t[i].setName(Integer.toString(random.nextInt(10000)));
+        }
+        for (int i = 0; i < NUMTHREADS; ++i) {
             t[i].start();
+        }
+        // Give it some time to run...
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        for (int i = 0; i < NUMTHREADS; i++)
+        {
+            System.out.println(pQueue.getFirst());
+        }
+    }
+
+    public static void testOrder(PriorityQueue pQueue)
+    {
+        pQueue.add("A", 9);
+        pQueue.add("B", 9);
+        for (int i = 0; i < 2; i++){
+            System.out.println(pQueue.getFirst());
         }
     }
 }
